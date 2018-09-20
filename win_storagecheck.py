08b9519eb33f88db44c110cd726f3627e79f5442
m_miyazaki <planet.titan@gmail.com>
2018-09-20 22:02:41 +0900

    Fix a typo

--- win_storagecheck.py
+++ win_storagecheck.py
@@ -18,7 +18,7 @@ def remin_hdd(drive):
 def main():
     hdd = remin_hdd('N:\\')
     today = datetime.datetime.now()
-    if hdd[1] < 200:
+    if hdd[1] < 100:
         notify.line('録画HDDの残りが少なくなりました。\nHDDを整理しましょう。残{:.2f}GBです。'.format(hdd[1]))
     elif today.isoweekday() == 4:
         notify.line('録画HDDの残りは{:.2f}GBです。'.format(hdd[1]))
