d0129dd9ac91025c9d3bf0fde9c4a9eac43ba05f
m_miyazaki <planet.titan@gmail.com>
2018-09-23 11:18:19 +0900

    Make some values ​​variable and Update README.md

--- README.md
+++ README.md
@@ -1,3 +1,37 @@
 # remaining HDD
 ハードディスクの容量が少なくなったら、LINEにアラートを送信するやつ。
 
+## 使い方
+1. Python3を使えるようにする
+自身のWindowsにpyhtonをインストール。バージョンは3.4以上であれば大丈夫だと思います。
+
+### インストール
+```
+> git clone https://github.com/titanium99/notificate.git
+> cd notificate
+```
+
+### 必要なモジュール追加
+```python3
+> pip install -f requirements.txt
+```
+
+### LINE Notifyでアクセストークンを発行する
+ログイン後、マイページの「アクセストークンの発行」の「トークンを発行」から発行する。
+「サービスを登録」と間違いやすいので注意。
+また、トークンは１回発行したら再発行できないので注意。
+忘れてしまったら既存のnotifyを削除し、再度作成し直し。
+
+### iniファイルにアクセストークンを転記
+linenotify.ini.sampleのファイル名の".sample"以降を削除して、自身のアクセストークンを転記する。
+
+### テスト
+コマンドプロンプト等でwin_storagechek.pyを実行して、LINEに通知が行くかどうか確認する。
+
+### win_storagechek.pyの編集
+main()内のhdd_path,alert_GB,check_dayを自身の環境に応じ修正。
+
+### Windowsのタスクスケジューラに登録
+自分は1日ごとの実行で良いと思う。この辺はお好きなように。
+
+
--- win_storagecheck.py
+++ win_storagecheck.py
@@ -11,16 +11,27 @@ def remin_hdd(drive):
     """
     # HDDの使用量を取得。返り値：total,used,free
     du = shutil.disk_usage(drive)
+    # 値はバイトなのでGB表示に変換。
+    # 今回usedはいらないので計算せず。
     rem_GB = du.free/1024**3
     total_GB = du.total/1024**3
     return total_GB, rem_GB
 
 def main():
-    hdd = remin_hdd('N:\\')
+    """ 1日1回のチェック。
+    指定した容量以下ならアラートを送信。
+    指定した容量以上でも週１回は残量を送信。
+    """
+    hdd_path = 'N:\\' # チェックするHDDのパス。Nドライブ
+    alert_GB = 100 # アラートする残量。100GB以下でアラート
+    check_day = 4 # チェックする曜日(月曜1~日曜7)。木曜になったら残量の連絡。
+    
+    hdd = remin_hdd(hdd_path)
     today = datetime.datetime.now()
-    if hdd[1] < 100:
+    if hdd[1] < alert_GB:
+        # メッセージは適当に変えてください。
         notify.line('録画HDDの残りが少なくなりました。\nHDDを整理しましょう。残{:.2f}GBです。'.format(hdd[1]))
-    elif today.isoweekday() == 4:
+    elif today.isoweekday() == check_day:
         notify.line('録画HDDの残りは{:.2f}GBです。'.format(hdd[1]))
     
 if __name__ == '__main__':
